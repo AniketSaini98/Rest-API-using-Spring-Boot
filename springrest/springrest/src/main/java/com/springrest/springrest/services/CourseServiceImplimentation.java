5c96f3473831760e4f8257489309a88d2c0bd1f2
Aniket Saini <103487375+AniketSaini98@users.noreply.github.com>
2022-10-14 13:43:01 +0530

    Update CourseServiceImplimentation.java
    Remove the unwanted code - Second Commit

--- springrest/springrest/src/main/java/com/springrest/springrest/services/CourseServiceImplimentation.java
+++ springrest/springrest/src/main/java/com/springrest/springrest/services/CourseServiceImplimentation.java
@@ -12,12 +12,8 @@ public class CourseServiceImplimentation implements CourseServices {
 
 	@Autowired
 	private CourseDao courseDao;
-	//List<Courses> list;
 	
 	public CourseServiceImplimentation() {
-//		list = new ArrayList<>();
-//		list.add(new Courses(10, "FrontEnd Development", "This course contains Basic Concepts of FrontEnd Development"));
-//		list.add(new Courses(20, "BackEnd Development", "This course contains Basic Concepts of BackEnd Development"));
 	}
 	
 	@Override
@@ -28,37 +24,17 @@ public class CourseServiceImplimentation implements CourseServices {
 	@SuppressWarnings("deprecation")
 	@Override
 	public Courses getCourse(long courseId) {
-		
-//		Courses c = null;
-//		
-//		for(Courses course : list) {
-//			
-//			if(course.getId() == courseId) {
-//				c = course;
-//				break;
-//			}
-//		}
-		
 		return courseDao.getOne(courseId);
 	}
 
 	@Override
 	public Courses addCourse(Courses course) {
-//		list.add(course);
 		courseDao.save(course);
 		return course;
 	}
 
 	@Override
 	public Courses updateCourse(Courses course) {
-		
-//		list.forEach(e -> {
-//			if(e.getId() == course.getId()) {
-//				e.setCourseName(course.getCourseName());
-//				e.setCourseDescription(course.getCourseDescription());
-//			}
-//		});
-		
 		courseDao.save(course);
 		return course;
 	}
@@ -66,7 +42,6 @@ public class CourseServiceImplimentation implements CourseServices {
 	@SuppressWarnings("deprecation")
 	@Override
 	public void deleteCourse(long parseLong) {
-//		list = this.list.stream().filter(e -> e.getId() != parseLong).collect(Collectors.toList());
 		Courses entity = courseDao.getOne(parseLong);
 		courseDao.delete(entity);
 	}
